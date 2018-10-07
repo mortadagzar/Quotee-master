c4872dd0b3dd16d67273289b272ea6638292858e
mortada gzar <mortadagzar@gmail.com>
2018-10-06 17:05:00 -0700

    css

--- static/blog/js/js.js
+++ static/blog/js/js.js
@@ -5,4 +5,20 @@
   });
 
 
-   
+  $(window).scroll(function() {
+ 
+    if ($(this).scrollTop()>50)
+     {
+        $('.button-out').stop().animate({ // menu goes out
+            marginLeft:' 79%',
+            transform:' scale(1.2)',
+           
+            
+        });
+
+    
+     }
+    
+     
+
+ });
