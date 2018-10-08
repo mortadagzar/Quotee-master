7f81bafec05ce35cb2ca2d72c92cb8d7767d348d
mortada gzar <mortadagzar@gmail.com>
2018-10-07 22:21:34 -0700

    ghi

--- static/blog/js/js.js
+++ static/blog/js/js.js
@@ -1,30 +1,49 @@
+$(document).ready(function() {
+  // Optimalisation: Store the references outside the event handler:
+  var $window = $(window);
+  var $pane = $('.owl');
+
+  function checkWidth() {
+      var windowsize = $window.width();
+      if (windowsize > 440) {
+         
+        $( '.owl' ).css( "left", "12%" );
+        $( '.owl' ).css( "width", "72px" );
+
+      }
+      
+  }
+  // Execute on load
+  checkWidth();
+  // Bind event listener
+  $(window).resize(checkWidth);
+});
+
+
+
 $(function(){
-    var $elems = $('.tto');
-    var winheight = $(window).height();
-    var fullheight = $(document).height();
-   
-    $(window).scroll(function(){
-      animate_elems();
-    });
-  
+  var $elems = $('.quoteb');
+  var winheight = $(window).height();
+  var fullheight = $(document).height();
+ 
+  $(window).scroll(function(){
+    animate_elems();
+  });
 
   function animate_elems() {
     wintop = $(window).scrollTop(); // calculate distance from top of window
  
     // loop through each item to check when it animates
     $elems.each(function(){
-        $elm = $(this);
+      $elm = $(this);
  
  
       topcoords = $elm.offset().top; // element's distance from top of page in pixels
  
-      if(wintop<topcoords) {
+      if(wintop >200) {
         // animate when top of the window is 3/4 above the element
-        $elm.animate({
-
-            opacity:1, 
-        })
-      }
+        $( '.quoteb' ).css( "opacity", "1" );      }
     });
   } // end animate_elems()
 });
+
